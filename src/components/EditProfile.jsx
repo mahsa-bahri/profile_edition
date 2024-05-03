b30a00525a45f96d6706a2cf674abf6f4ac98ae9
Mahsa_bhr <mahsabahri79@gmail.com>
2024-05-03 17:26:24 +0330

    Implemented new feature for user profile

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,10 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import { IoArrowBack } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
-export default function EditProfile (){
-  
-  return(<div>
-    <form>
-      
-    </form>
-  </div>);
+export default function EditProfile() {
+  const [formData, setFormData] = useState({
+    name: "mahsa ",
+    familyName: "",
+    email: "",
+    birthdate: "",
+    phoneNumber: "",
+    gender: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    console.log(formData);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission here
+    console.log(formData);
+  };
+
+  return (
+    <div className="profile-page" >
+      <p className="profile-title"><Link to="/profile"><IoArrowBack color="white"/></Link> Edit Profile</p>
+      <form onSubmit={handleSubmit} className="profile-edit-form">
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          placeholder={formData.name}
+        />
+        <br />
+        <input
+          type="text"
+          name="familyname"
+          value={formData.familyName}
+          onChange={handleChange}
+          placeholder={formData.familyName}
+        />
+        <br />
+        <input
+          type="date"
+          name="birthdate"
+          value={formData.birthdate}
+          onChange={handleChange}
+          placeholder={formData.birthdate}
+        />
+        <br />
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder={formData.email}
+        />
+        <br />
+        <input
+          type="tel"
+          name="phoneNumber"
+          value={formData.phoneNumber}
+          onChange={handleChange}
+          placeholder={formData.phoneNumber}
+
+        />
+        <br />
+        <select name="gender" value={formData.gender} onChange={handleChange} >
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+        <br />
+        <button type="submit">update</button>
+      </form>
+    </div>
+  );
 }
--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -2,31 +2,34 @@ import React from "react";
 import "./ProfilePage.css";
 import { Link } from "react-router-dom";
 import pic from "../assets/profile.jpg";
+import { CgProfile } from "react-icons/cg";
+import { MdPayment } from "react-icons/md";
+import { GoShieldCheck } from "react-icons/go";
+import { IoLogOutOutline } from "react-icons/io5";
 
 export default function ProfilePage() {
   return (
     <div className="profile-page">
+      <p className="profile-title">Profile</p>
       <div className="personal-data">
-        <img src={pic} className="profile-pic mx-auto" />
-        <p class="text-xl">Mahsa Bahri</p>
-        <p class="text-sm">mahsabahri79@gmail.com</p>
+        <img src={pic} className="profile-pic " />
+        <p className="profile-name">Mahsa Bahri</p>
+        <p className="text-sm text-3xl font-bold mb-4">mahsabahri79@gmail.com</p>
       </div>
 
       <nav className="navigation">
         <ul>
           <li className="navigation">
-            <Link to="/edit-profile" className="no-underline ...">
-              edit profile
-            </Link>
+            <Link to="/edit-profile" className="no-underline ..."><div className="profile-item"><CgProfile color="white" /> <span>edit profile</span></div></Link>
           </li>
           <li>
-            <Link to="/payment">payment</Link>
+            <Link to="/payment"><div className="profile-item"><MdPayment color="white"/><span> payment </span></div></Link>
           </li>
           <li>
-            <Link to="/security">security</Link>
+            <Link to="/security"><div className="profile-item"><GoShieldCheck color="white"/><span> security</span></div></Link>
           </li>
           <li>
-            <Link to="/logout">log out</Link>
+            <Link to="/logout"><div className="profile-item"><IoLogOutOutline color="red"/><span style={{color:'red'}}> log out</span></div></Link>
           </li>
         </ul>
       </nav>
